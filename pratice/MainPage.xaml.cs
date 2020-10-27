909e3c8dd9692c7966bb51f22f25b3ac9cb34e62
zzduckyzz <duc.phamminh94@gmail.com>
2020-10-27 09:28:14 +0700

    practical

---
+++ pratice/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using pratice.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace pratice
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<IconSS> Icons;
+        private ObservableCollection<Contact> Contacts;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Icons = new List<IconSS>();
+
+            Icons.Add(new IconSS { iconPath = "Assets/img-01.jpg" });
+            Icons.Add(new IconSS { iconPath = "Assets/img-02.jpg" });
+            Icons.Add(new IconSS { iconPath = "Assets/img-03.jpg" });
+  
+            Contacts = new ObservableCollection<Contact>();
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            string avatar = ((IconSS)Images.SelectedValue).iconPath;
+            //Thêm item mới
+            Contacts.Add(new Contact { FirstName = FirstNameTextBox.Text, LastName = LastNameTextBox.Text, AvatarPath = avatar });
+
+            //Reset data
+            FirstNameTextBox.Text = "";
+            LastNameTextBox.Text = "";
+
+            //AvatarComboBox.SelectedItem = -1;
+            FirstNameTextBox.Focus(FocusState.Programmatic);
+        }
+    }
+}
